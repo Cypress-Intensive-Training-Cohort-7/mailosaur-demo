cd6fb230b9eeb5d45acbd2dc086f1f217501d49b
Ademola Bhadmus <bhadmusautomates1@gmail.com>
2025-05-10 19:10:24 +0100

    removed sensitive file

--- cypress.config.js
+++ cypress.config.js
@@ -8,6 +8,9 @@
           viewportHeight: 960,
           viewportWidth: 1530,
           chromeWebSecurity: false,
+          // env:{
+          //   "MAILOSAUR_API_KEY": ""
+          // },
           setupNodeEvents(on, config) {
             // implement node event listeners here
           },
